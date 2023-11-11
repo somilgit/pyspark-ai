bc7557fc6e1a7404a4159d9716ce675a929f5845
somilgit <122661445+somilgit@users.noreply.github.com>
2023-11-12 00:20:29 +0530

    Update pyspark_ai.py

--- pyspark_ai/pyspark_ai.py
+++ pyspark_ai/pyspark_ai.py
@@ -536,6 +536,7 @@ class SparkAI:
                  on the input DataFrame.
         """
         sql_query = self._get_transform_sql_query(df, desc, cache)
+        print("@@@@@@@{}@@@@@@@@".format(sql_query))
         return self._spark.sql(sql_query)
 
     def explain_df(self, df: DataFrame, cache: bool = True) -> str:
